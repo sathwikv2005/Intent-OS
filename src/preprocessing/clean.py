4a6428bc7e3552fa264ba38bbc397ef53e464e92
sathwikv2005 <sathwikv2005@gmail.com>
2026-04-07 18:33:22 +0530

    add string normalisation

--- src/preprocessing/clean.py
+++ src/preprocessing/clean.py
@@ -2,13 +2,15 @@ import re
 
 def clean_text(text: str) -> str:
     """
-     Converts to lowercase and cleans text of punctuation, special chars, extra spaces.
+     Converts to lowercase, normalises it, cleans text of punctuation, special chars, extra spaces.
     """
 
     text = text.lower()
+    
+    text = text.replace("'s", "")               # normalise 's 
 
-    text = re.sub(r'\s+', ' ', text).strip() # remove extra white spaces
+    text = re.sub(r'\s+', ' ', text).strip()    # remove extra white spaces
 
-    text = re.sub(r'[^a-z0-9\s]', '', text) # remove any char not a alphabet, number or whitespace.
+    text = re.sub(r'[^a-z0-9\s]', '', text)     # remove any char not a alphabet, number or whitespace.
 
     return text
