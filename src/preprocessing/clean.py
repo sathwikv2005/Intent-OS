1fa550c179a2f609eb1f09547e8742dde91ebadd
sathwikv2005 <sathwikv2005@gmail.com>
2026-04-07 20:02:46 +0530

    dont not clean '%' symbol

--- src/preprocessing/clean.py
+++ src/preprocessing/clean.py
@@ -11,6 +11,6 @@ def clean_text(text: str) -> str:
 
     text = re.sub(r'\s+', ' ', text).strip()    # remove extra white spaces
 
-    text = re.sub(r'[^a-z0-9\s]', '', text)     # remove any char not a alphabet, number or whitespace.
+    text = re.sub(r'[^a-z0-9\s%]', '', text)     # remove any char not a alphabet, number or whitespace.
 
     return text
