e615b29dd0ee9a3117f7efd0291d4e6f9b8fe49b
sathwikv2005 <sathwikv2005@gmail.com>
2026-04-07 23:40:36 +0530

    retry login if it fails

--- src/core/vtopClient.py
+++ src/core/vtopClient.py
@@ -1,6 +1,6 @@
 import requests
 
-BASE_URL = "http://localhost:6700/api/"  # your Node API
+BASE_URL = "http://localhost:6700/api"  # your Node API
 
 class vtopClient:
     def __init__(self):
@@ -20,18 +20,22 @@ class vtopClient:
         return {"success" : True}
 
     def getSem(self):
-        res = requests.get(f"{BASE_URL}/semids")
+        res = requests.get(f"{BASE_URL}/semids?jsessionId={self.cookie}&csrf={self.csrf}")
         data = res.json()
+        
+        
         if(res.status_code != 200):
             return {"success" : False, "error": data.get("error", "Login failed")}
         
-        self.sem = data["semesters"][0].semId
+        self.sem = data[0]["semId"]
         return {"success" : True}
 
     def login(self, tries = 0):
+        print(f"Logging into VTOP tries: {tries}.")
         response = requests.post(f"{BASE_URL}/login/autocaptcha")
         
         if response.status_code == 500:
+                print("Login failed.")
                 return {"success" : False, "error": "VTOP server might be down"}
 
         data = response.json()
@@ -40,39 +44,57 @@ class vtopClient:
 
                 if ("csrf" in error or "captcha" in error) and tries < 5:
                     return self.login(tries + 1)
-
+                
+                print("Login failed.")
                 return {"success" : False, "error": data.get("error", "Login failed")}
+        
 
-        self.csrf = data.csrf
+        
+        self.csrf = data.get("csrf", None)
         self.cookie = next((c["value"] for c in data.get("cookies", []) if c.get("key") == "JSESSIONID"),None)
+
+
+        if(self.csrf == None or self.cookie == None):
+             return self.login(tries + 1)
+        
 
         res = self.getSem()
         if res.get("error"):
+             print("Login failed.")
              return {"success" : False, "error": res.get("error", "Login failed")}
+        
+
+        print("Logged into VTOP.")
         return {"success" : True}
       
 
     def getTimeTable(self):
         if self._timetable is not None:
             return {"success" : True, "timetable": self._timetable}
+        
+        self.checkLogin()
         res = requests.get(f"{BASE_URL}/timetable?jsessionId={self.cookie}&csrf={self.csrf}")
         data = res.json()
         if(res.status_code != 200):
               return {"success" : False, "error": data.get("error", "Timetable fetch failed")}
         
-        timetable = data.get("timeTable", [])
+        
+        timetable = data
         self._timetable = timetable
         return {"success" : True, "timetable": timetable}
     
 
     def getAttendance(self):
         if self._attendance is not None:
-            return {"success" : True, "timetable": self._attendance}
-        res = requests.get(f"{BASE_URL}/timetable?jsessionId={self.cookie}&csrf={self.csrf}&semID={self.sem}")
+            return {"success" : True, "attendance": self._attendance}
+        
+        self.checkLogin()
+        res = requests.get(f"{BASE_URL}/attendance?jsessionId={self.cookie}&csrf={self.csrf}&semID={self.sem}")
         data = res.json()
         if(res.status_code != 200):
               return {"success" : False, "error": data.get("error", "Attendance fetch failed")}
         
-        attendance = data.get("attendance", [])
+        
+        attendance = data
         self._attendance = attendance
         return {"success" : True, "attendance": attendance}
