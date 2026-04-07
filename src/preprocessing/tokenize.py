8aa13b17eb81b2f0001e668e8fee470c0a109f45
sathwikv2005 <sathwikv2005@gmail.com>
2026-04-07 17:52:15 +0530

    add stop word removal

--- src/preprocessing/tokenize.py
+++ src/preprocessing/tokenize.py
@@ -1,5 +1,11 @@
+STOPWORDS = {
+    "my", "the", "is", "are", "for", "a", "an", "to", "of"
+}
+
 def tokenize(text: str) -> list:
     """
     Converts cleaned text into tokens (words)
-    """
-    return text.split()
+   """
+    tokens = text.split()
+    tokens = [word for word in tokens if word not in STOPWORDS]
+    return tokens
